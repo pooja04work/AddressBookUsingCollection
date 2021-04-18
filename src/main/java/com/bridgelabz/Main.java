fe244486a1b131da293d405785435095c50d8f37
Pooja <pooja.14206@gmail.com>
2021-04-18 10:03:57 +0530

    [POOJA] added a file to store contact inside addressbook

--- src/main/java/com/bridgelabz/Main.java
+++ src/main/java/com/bridgelabz/Main.java
@@ -1,7 +1,11 @@
 package com.bridgelabz;
 
+import com.bridgelabz.com.bridgelabz.com.bridgelabz.AddressBook;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Wellcome to Address Book program.");
+        AddressBook storeValue = new AddressBook("john", "kennedy", "Gauhati", "Assam", "xyz14@gmail.in", 78325, 100);
+        storeValue.Display();
     }
 }
