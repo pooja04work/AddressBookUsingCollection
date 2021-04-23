2fc8f27f677ff2c58a36b853445957643a932104
Pooja <pooja.14206@gmail.com>
2021-04-23 14:33:49 +0530

    adding a file which count the number of people in a city

---
+++ src/main/java/com/bridgelabz/AddressBook.java
@@ -0,0 +1,162 @@
+package com.bridgelabz;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
+public class AddressBook {
+    Scanner scanner = new Scanner(System.in);
+    List<ContactPerson> list = new ArrayList();
+    AddressBookManager addressBookManager = new AddressBookManager();
+
+    public void createAddressbook() {
+        addressBookManager.CreateAddressbook();
+    }
+    public void getContacts() {
+        System.out.println("Available adressbooks are" + addressBookManager.addressbook.keySet());
+        System.out.println("from which addressbook you want to find details?? ");
+        String addressBookName = scanner.next();
+        addressBookManager.getContactByAddressBook(addressBookName);
+    }
+    /**
+     * @mathod add() add new person and their contact detail to the address book
+     * @return list contain all the details of the persons newly added.
+     */
+    public List<ContactPerson> add() {
+        System.out.println("Add new Contact to ADDRESS BOOK:--");
+        System.out.println("Available adressbooks are" + addressBookManager.addressbook.keySet());
+
+        System.out.println("Enter the AddressBook Name: ");
+        String addressBookName = scanner.next();
+        System.out.println("enter the number of people you want to add in addressbook: ");
+        int numberOfUser = Integer.valueOf(scanner.next());
+
+        for (int i = 0; i < numberOfUser; i++) {
+            ContactPerson person = new ContactPerson();
+            System.out.println("First Name: ");
+            String firstName = scanner.next();
+            person.setFirstName(firstName);
+
+            System.out.println("Last Name: ");
+            String lastName = scanner.next();
+            person.setLastName(lastName);
+
+            System.out.println("City: ");
+            String city = scanner.next();
+            person.setCity(city);
+
+            System.out.println("State: ");
+            String state = scanner.next();
+            person.setState(state);
+
+            System.out.println("EmailID: ");
+            String email = scanner.next();
+            person.setEmail(email);
+
+            System.out.println("Zip: ");
+            int zip = scanner.nextInt();
+            person.setZip(zip);
+
+            System.out.println("PhoneNumber: ");
+            int phoneNo = scanner.nextInt();
+            person.setPhoneNo(phoneNo);
+
+            addressBookManager.addContact(addressBookName, person);
+            list.add(person);
+        }
+        return list;
+    }
+    /**
+     * @mathod edit() perform edit operation in existing Contact of address book.
+     * return the list after editing with new edited value.
+     *return the list after editing with new edited value.
+     */
+
+
+
+    public void edit() {
+        System.out.println("Add new Contact to ADDRESS BOOK:--");
+        System.out.println("Available adressbooks are" + addressBookManager.addressbook.keySet());
+        System.out.println("Enter the AddressBook Name: ");
+        String addressBookName = scanner.next();
+
+        System.out.println("Enter your First name:");
+        String fname = scanner.next();
+        List list = this.list;
+
+        Iterator iterator = list.iterator();
+
+        while (iterator.hasNext()) {
+            ContactPerson person = (ContactPerson) iterator.next();
+
+            if (fname.equals(person.getFirstName())) {
+
+                System.out.println("Choose field you want to add:");
+                System.out.println("1.Last Name\t2.Phone Number\t3.City\t4.Zip\t5. State");
+
+                switch (scanner.nextInt()) {
+                    case 1:
+                        System.out.println("Re-Correct your lastname");
+                        person.lastName = scanner.next();
+                        System.out.println("new lastname: " + person.lastName);
+                        break;
+                    case 2:
+                        System.out.println("Re-Correct your Phone Number");
+                        person.phoneNo = scanner.nextInt();
+                        System.out.println("new PhoneNo: " + person.phoneNo);
+                        break;
+                    case 3:
+                        System.out.println("Re-Correct your City");
+                        person.city = scanner.next();
+                        System.out.println("new city: " + person.city);
+                        break;
+                    case 4:
+                        System.out.println("Re-Correct your Zip");
+                        person.zip = scanner.nextInt();
+                        System.out.println("new zip: " + person.zip);
+                        break;
+                    case 5:
+                        System.out.println("Re-Correct your State");
+                        person.state = scanner.next();
+                        System.out.println("new state: " + person.state);
+                        break;
+                }
+            }
+        }
+
+    }
+    /**
+     * @method delete() deleted a person detail from the Addressbook using Name
+     * @return list after deleting
+     */
+    public void delete() {
+        System.out.println("Enter your First name:");
+        String fname = scanner.next();
+        Iterator iterator = list.iterator();
+        while (iterator.hasNext()) {
+            ContactPerson person = (ContactPerson) iterator.next();
+
+            if (fname.equals(person.getFirstName())) {
+                list.remove(person);
+            }
+        }
+    }
+
+    public void searchByCityOrState() {
+        int count =0;
+        System.out.println("All the people belongs to City = kokrajhar");
+        for (int index = 0; index <list.size(); index++){
+            if (list.get(index).getCity().equals("kokrajhar")){
+                System.out.println(list.get(index) );
+                count = index;
+            }
+        }
+        System.out.println("number of contact person is : "+ count + "where city = kokrajhar");
+    }
+
+//    public void sortByNameAlpha() {
+//    	List result = list.stream().sorted((o1, o2)->o1.getFirstName().getValue().compareTo(o2.getFirstName().getValue())).collect(Collectors.toList());
+//    }
+
+}
---
+++ src/main/java/com/bridgelabz/Main.java
@@ -0,0 +1,62 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Wellcome to Address Book program.");
+        ContactPerson storeValue = new ContactPerson("john", "kennedy", "Gauhati", "Assam", "xyz14@gmail.in", 78325, 100);
+        Scanner scanner = new Scanner(System.in);
+        AddressBook addressBook = new AddressBook();
+        AddressBookManager addressBookManager = new AddressBookManager();
+
+        storeValue.Display();
+
+        int switchValue = 1;
+        while(switchValue != 0) {
+            System.out.println("Select a Option: ");
+            System.out.println("1.New AddressBook\n2.add on exisiting Address Book\n"
+                    + "3.get the contact detail\n4.edit contact detail\n"
+                    + "5.delete contact details\n 6.search contact by city\n"
+                    + "8.exit" );
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+
+                case 1:
+                    addressBook.createAddressbook();
+                    break;
+
+                case 2:
+                    addressBook.add();
+                    break;
+
+                case 3:
+                    addressBook.getContacts();
+                    break;
+
+                case 4:
+                    addressBook.edit();
+                    break;
+
+                case 5:
+                    addressBook.delete();
+                    break;
+
+                case 6:
+                    addressBook.searchByCityOrState();
+                    break;
+
+//                case 7:
+//                	addressBook.viewPersonByCity();
+//                	break;
+//                "7.stor and retrive according city name.\n
+
+                case 8:
+                    System.out.println("Thank U! visit again.");
+                    switchValue = 0;
+            }
+        }
+
+    }
+}
